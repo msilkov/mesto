53218d7b324d30736464058cf006a48bfae23ddc
Michael <m-silkov@inbox.ru>
2022-06-04 23:29:55 +0300

    fix: fix all bugs

--- script/index.js
+++ script/index.js
@@ -1,25 +1,24 @@
 const editButton = document.querySelector(".profile__edit-btn");
 const popup = document.querySelector(".popup");
+const popupForm = popup.querySelector(".popup__form");
 const closeButton = popup.querySelector(".popup__close-btn");
 const popupName = popup.querySelector(".popup__field_type_name");
 const popupProfession = popup.querySelector(".popup__field_type_profession");
 const profileName = document.querySelector(".profile__name");
 const profileProfession = document.querySelector(".profile__desc");
 
-popupName.value = profileName.textContent;
-popupProfession.value = profileProfession.textContent;
-
 const popupOpen = function (e) {
 	e.preventDefault();
 	popup.classList.add("popup_opened");
-  document.body.style.overflowY = 'hidden';
+	document.body.style.overflowY = "hidden";
+	popupName.value = profileName.textContent;
+	popupProfession.value = profileProfession.textContent;
 };
 
 const popupClose = function (e) {
 	e.preventDefault();
 	popup.classList.remove("popup_opened");
-  document.body.style.overflowY = 'auto';
-
+	document.body.style.overflowY = "auto";
 };
 
 const formSubmitHandler = function (e) {
@@ -27,9 +26,9 @@ const formSubmitHandler = function (e) {
 	profileName.textContent = popupName.value;
 	profileProfession.textContent = popupProfession.value;
 	popup.classList.remove("popup_opened");
-  document.body.style.overflowY = 'auto';
+	document.body.style.overflowY = "auto";
 };
 
 editButton.addEventListener("click", popupOpen);
 closeButton.addEventListener("click", popupClose);
-popup.addEventListener("submit", formSubmitHandler);
+popupForm.addEventListener("submit", formSubmitHandler);
